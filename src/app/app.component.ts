1c55c51d4aa8876bf501b8bd26557847184124c7
Meritxell Bendicho <meritxey@gmail.com>
2021-11-09 18:47:48 +0100

    last change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-// Otro comentario
-// Esto es un comentario de la rama navbar
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
